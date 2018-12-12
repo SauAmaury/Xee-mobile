f8e797990511e0ee183e7c4157bbbf983c679588
diabolo52 <diabolo52000@hotmail.fr>
2018-12-12 23:24:41 +0100

    Intégration du SDK + Tests

--- app/src/main/java/cnam/myapplication/MainActivity.java
+++ app/src/main/java/cnam/myapplication/MainActivity.java
@@ -2,6 +2,14 @@ package cnam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.xee.sdk.api.XeeApi;
+import com.xee.sdk.core.auth.AuthenticationCallback;
+import com.xee.sdk.core.auth.XeeAuth;
+import com.xee.sdk.core.common.XeeEnv;
+
+import cnam.myapplication.XeeDev.XeeConnect;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        XeeConnect xe = new XeeConnect();
+        XeeEnv xeeEnv = new XeeEnv(this, xe.getoAuthClient());
+        XeeApi xapi = new XeeApi(xeeEnv, true);
+        XeeAuth xeau = new XeeAuth(xeeEnv, true);
+       /* AuthenticationCallback authcall = new AuthenticationCallback() {
+            @Override
+            public void onError(Throwable throwable) {
+                Log.i("Erreur",throwable.getMessage());
+            }
+
+            @Override
+            public void onSuccess() {
+                Log.i("ok","Connexion réussi !");
+            }
+        };
+        xeau.connect(authcall);*/
+
+
     }
 }
