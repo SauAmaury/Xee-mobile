444220a2de2181d20d26eb8e999c297563a84a90
diabolo52 <diabolo52000@hotmail.fr>
2018-12-13 22:57:23 +0100

    Mise en place de la connexion

--- app/src/main/java/cnam/myapplication/MainActivity.java
+++ app/src/main/java/cnam/myapplication/MainActivity.java
@@ -3,37 +3,66 @@ package cnam.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.xee.sdk.api.XeeApi;
+import com.xee.sdk.api.model.Authorization;
+import com.xee.sdk.api.model.User;
+import com.xee.sdk.api.model.Vehicle;
 import com.xee.sdk.core.auth.AuthenticationCallback;
+import com.xee.sdk.core.auth.OAuth2Client;
 import com.xee.sdk.core.auth.XeeAuth;
 import com.xee.sdk.core.common.XeeEnv;
 
-import cnam.myapplication.XeeDev.XeeConnect;
+import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.Observable;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Button logIn;
+    private XeeEnv xenv;
+    private XeeAuth xeau;
+    private XeeApi xapi;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        XeeConnect xe = new XeeConnect();
-        XeeEnv xeeEnv = new XeeEnv(this, xe.getoAuthClient());
-        XeeApi xapi = new XeeApi(xeeEnv, true);
-        XeeAuth xeau = new XeeAuth(xeeEnv, true);
-       /* AuthenticationCallback authcall = new AuthenticationCallback() {
-            @Override
-            public void onError(Throwable throwable) {
-                Log.i("Erreur",throwable.getMessage());
-            }
+        this.logIn =  findViewById(R.id.sign_in_button);
 
+        OAuth2Client oAuthClient = new OAuth2Client.Builder()
+                .clientId("324b3e00-2ccf-477e-833e-b1c819c6577f")
+                .clientSecret("b0aeab4367810e479a4166c1f6668ccffcb1b0a492d8eb92a28699516c2f3751")
+                .scopes(Arrays.asList("vehicles.read"))
+                .build();
+
+
+
+        this.xenv = new XeeEnv(this, oAuthClient);
+        this.xapi = new XeeApi(this.xenv, true);
+        this.xeau = new XeeAuth(this.xenv, true);
+
+
+    }
+
+    public void logIn(View v)
+    {
+        xeau.connect(new AuthenticationCallback() {
+            @Override
+            public void onError(@NotNull Throwable throwable) {
+                Log.i("RES-Errreur",throwable.getMessage());
+            }
             @Override
             public void onSuccess() {
-                Log.i("ok","Connexion réussi !");
+                Log.i("RES-Ok","Utilisateur connecté");
             }
-        };
-        xeau.connect(authcall);*/
-
-
+        });
     }
 }
